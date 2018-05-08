98eba7e3d63be081d9ced877a0298d93939acf20
Neprine <pjonathanvd@gmail.com>
2018-05-08 18:22:29 +0200

    Initial commit

---
+++ src/RLE.java
@@ -0,0 +1,85 @@
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.lang.reflect.Array;
+import java.util.ArrayList;
+import java.util.List;
+
+public class RLE {
+
+    public static void compress(InputStream is, OutputStream os) throws Exception {
+        int valor;
+        int ultimo = -1;
+        int contador = 0;
+        System.out.println("NUEVO");
+        while ((valor = is.read()) != -1) {
+
+            if (ultimo == valor) {
+                contador++;
+                if (contador == 1) {
+                    os.write(ultimo);
+                    System.out.println(ultimo);
+
+                } else if (contador >= 255) {
+                    os.write(contador);
+                    os.write(ultimo);
+                    System.out.println(contador);
+                    System.out.println(ultimo);
+                    contador = -2;
+                }
+
+            } else {
+                if (ultimo != -1 && contador >= 2) {
+                    os.write(contador - 1);
+                    System.out.println(contador - 1);
+
+                } else if (contador == 1) {
+                    os.write(0);
+                    System.out.println(0);
+                }
+                ultimo = valor;
+                System.out.println(ultimo);
+                os.write(ultimo);
+                contador = 0;
+            }
+
+        }
+
+        if (contador != 0) {
+            os.write(contador - 1);
+            System.out.println(contador - 1);
+
+        }
+
+        System.out.println("cierra");
+        os.close();
+        is.close();
+
+
+    }
+
+
+    public static void decompress(InputStream is, OutputStream os) throws Exception {
+        int valor;
+        int ultimo = -1;
+        int contador = 0;
+        System.out.println("NUEVO");
+        while ((valor = is.read()) != -1) {
+            if (valor == 0) {
+                System.out.println("soy 0");
+            } else {
+                ultimo = valor;
+                System.out.println(ultimo);
+                os.write(ultimo);
+
+
+            }
+        }
+
+        System.out.println("cierra");
+        os.close();
+        is.close();
+
+
+    }
+
+}
